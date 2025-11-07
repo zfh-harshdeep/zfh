e9903b51efc84b7b39f47c74d439b4772c2216ad
zfh-harshdeep <zerofundedhustle@gmail.com>
2025-11-08 00:25:00 +0530

    Delete functions directory

--- functions/success.html.js
+++
@@ -1,7 +0,0 @@
-// Handle all requests to /success.html - serve the static success page
-export async function onRequest(context) {
-    const { request, env } = context;
-    
-    // Always serve the static success.html file for any request method
-    return env.ASSETS.fetch(request);
-}
