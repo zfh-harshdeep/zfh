ccf0f70284d4e13f318b9165f76f7718ee6bce17
zfh-harshdeep <zerofundedhustle@gmail.com>
2025-11-08 00:15:37 +0530

    Simplify success.html.js to handle all request methods

--- functions/success.html.js
+++ functions/success.html.js
@@ -1,23 +1,7 @@
-// Handle POST requests from PayU payment gateway to /success.html
+// Handle all requests to /success.html - serve the static success page
 export async function onRequest(context) {
-  const { request, env } = context;
-
-  // If this is a POST request, fetch and return the success.html file
-  // This allows payment gateways to POST payment data to this page
-  if (request.method === 'POST') {
-    // Fetch the static success.html file
-    const htmlResponse = await env.ASSETS.fetch(new URL('/success.html', request.url));
+    const { request, env } = context;
     
-    // Return the HTML with 200 status code
-    // Important: Don't redirect POST requests - serve the page directly
-    return new Response(htmlResponse.body, {
-      status: 200,
-      headers: {
-        'Content-Type': 'text/html;charset=UTF-8',
-      },
-    });
-  }
-
-  // For GET requests, serve the static file normally
-  return env.ASSETS.fetch(request);
+    // Always serve the static success.html file for any request method
+    return env.ASSETS.fetch(request);
 }
